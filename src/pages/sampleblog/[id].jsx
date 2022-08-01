8efb2f2761f874676ee5999f0ed384b14ef0eadd
toproad <tattun0817@gmail.com>
2022-08-01 13:02:20 +0900

    🎉 ブログ記事アイキャッチまで

---
+++ src/pages/sampleblog/[id].jsx
@@ -0,0 +1,41 @@
+import { client } from "../../lib/next/client";
+import React from "react";
+
+export default function BlogId({ blog }) {
+  console.log(blog);
+  return (
+    <main>
+      <h1>{blog.title}</h1>
+      <p>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+      />
+      <div>
+        
+        <img  src={blog.eyecatch.url}           alt="Picture of the author"           width={500}           height={500}         />
+      </div>
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "sampleblog" });
+  console.log("data");
+  const paths = data.contents.map((content) => `/sampleblog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "sampleblog", contentId: id });
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
